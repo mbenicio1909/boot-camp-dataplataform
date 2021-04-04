12354147d67ef6c01157acbc0107392338698108
Marcelo Benicio <mbenicio19@gmail.com>
2021-04-04 14:10:53 -0300

    Adicionando RDS e vpc

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from data_platform.data_lake.stack import DataLakeStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
-# common_stack = CommonStack(app)
+common_stack = CommonStack(app)
 # dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 # kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 # glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
