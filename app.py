5ec8b8f553795cd9e206772d816319f3960079e2
Marcelo Benicio <mbenicio19@gmail.com>
2021-04-04 12:53:46 -0300

    removido coisas nao utilizadas

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from data_platform.athena.stack import AthenaStack
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
-from data_platform.common_stack import CommonStack
-from data_platform.dms.stack import DmsStack
-from data_platform.kinesis.stack import KinesisStack
-from data_platform.glue_catalog.stack import GlueCatalogStack
-from data_platform.redshift.stack import RedshiftStack
-from data_platform.databricks.stack import DatabricksStack
+# from data_platform.common_stack import CommonStack
+# from data_platform.dms.stack import DmsStack
+# from data_platform.kinesis.stack import KinesisStack
+# from data_platform.glue_catalog.stack import GlueCatalogStack
+# from data_platform.redshift.stack import RedshiftStack
+# from data_platform.databricks.stack import DatabricksStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
