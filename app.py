ebbf84b409d3709d7bf2f817dff73fe0a0bd5ded
Marcelo Benicio <mbenicio19@gmail.com>
2021-04-21 21:49:08 -0300

    ajuste app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
 from data_platform.kinesis.stack import KinesisStack
 from data_platform.glue_catalog.stack import GlueCatalogStack
-# from data_platform.redshift.stack import RedshiftStack
-# from data_platform.databricks.stack import DatabricksStack
+from data_platform.redshift.stack import RedshiftStack
+from data_platform.databricks.stack import DatabricksStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
@@ -15,6 +15,6 @@ dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.da
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 athena = AthenaStack(app)
-# redshift = RedshiftStack(app, data_lake_raw=data_lake.data_lake_raw_bucket, data_lake_processed=data_lake.data_lake_processed_bucket, common_stack=common_stack)
-# databricks = DatabricksStack(app)
+redshift = RedshiftStack(app, data_lake_raw=data_lake.data_lake_raw_bucket, data_lake_processed=data_lake.data_lake_processed_bucket , common_stack=common_stack)
+databricks = DatabricksStack(app)
 app.synth()
